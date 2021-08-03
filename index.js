95763e30e20817160d05d46efd0ebf9a952542db
jiwonjung <4dakto@gmail.com>
2021-08-03 20:37:55 +0900

    register & login users

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
-const { apolloServer, ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
+const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 
-const Post = require('./models/Post');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config');
-
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (e) {
-        throw Error(e);
-      }
-    },
-  },
-};
 
 const server = new ApolloServer({
   typeDefs,
--- models/User.js
+++ models/User.js
@@ -1,11 +1,10 @@
-const { Model, Schema } = require('mongoose');
+const { model, Schema } = require('mongoose');
 
 const userSchema = new Schema({
   username: String,
   password: String,
   email: String,
-  createdAt: String
+  createdAt: String,
 });
 
-module.exports = modle('User', userSchema)
-
+module.exports = model('User', userSchema);
