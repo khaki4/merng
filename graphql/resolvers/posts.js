dad58414d3d25e2d51b39c650bbe32c01bbe7400
jiwonjung <4dakto@gmail.com>
2021-08-04 22:03:58 +0900

    Authentication middleware & Create/Delete posts

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,11 +1,56 @@
+const { AuthenticationError } = require('apollo-server');
 const Post = require('../../models/Post');
+const checkAuth = require('../../util/check-auth');
 
 module.exports = {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
+      } catch (e) {
+        throw Error(e);
+      }
+    },
+    async getPost(_, { postId }) {
+      try {
+        const post = await Post.findById(postId);
+        if (post) {
+          return post;
+        } else {
+          throw Error('Post not found');
+        }
+      } catch (e) {
+        throw Error(e);
+      }
+    },
+  },
+  Mutation: {
+    async createPost(_, { body }, context) {
+      const user = checkAuth(context);
+
+      const newPost = new Post({
+        body,
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString(),
+      });
+
+      const post = await newPost.save();
+
+      return post;
+    },
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return 'Post deleted successfully';
+        } else {
+          throw new AuthenticationError('Action not allowed');
+        }
       } catch (e) {
         throw Error(e);
       }
